d13df0d9c194cec348882b6940985c871fec2fce
Fabio Marturano <fabiocmjor@gmail.com>
2024-01-12 09:14:50 -0300

    refactory: revendo alguns conceitos e balancamento

--- Rabbit.Api/Controllers/RabbitController.cs
+++ Rabbit.Api/Controllers/RabbitController.cs
@@ -1,30 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Channels;
 
 namespace Rabbit.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class RabbitController(ILogger<RabbitController> logger) : ControllerBase
+    public class RabbitController : ControllerBase
     {
-        private readonly ILogger<RabbitController> _logger = logger;
+        private readonly ILogger<RabbitController> _logger;
+        private readonly IConnectionFactory _factory;
+
         private readonly string queue = "rabbit-queue";
         private readonly string queueError = "rabbit-queue-error";
-        private readonly List<Message> mensagens = new();
+
+        public RabbitController(ILogger<RabbitController> logger)
+        {
+            _logger = logger;
+            _factory = new ConnectionFactory { HostName = "localhost", UserName = "user", Password = "password" };
+        }
 
         [HttpPost("[action]")]
         public ActionResult<string> EnviarMensagem(int TotalMessagem)
         {
-
-            var factory = new ConnectionFactory { HostName = "localhost", UserName = "user", Password = "password" };
-            using var connection = factory.CreateConnection();
-            var channel = connection.CreateModel();
-            channel.QueueDeclare(queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            using var connection = _factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue, false, false, false, null);
 
             for (int i = 1; i <= TotalMessagem; i++)
             {
@@ -34,7 +37,7 @@ namespace Rabbit.Api.Controllers
                     DataEnvio = DateTime.Now,
                 };
 
-                channel.BasicPublish(exchange: string.Empty, routingKey: "rabbit-queue", basicProperties: null, body: Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message)));
+                channel.BasicPublish(string.Empty, "rabbit-queue", null, Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message)));
                 _logger.LogInformation($"Mensagem: \"{i}\" Sucesso");
             }
             return Ok();
@@ -42,35 +45,44 @@ namespace Rabbit.Api.Controllers
 
 
         [HttpGet("[action]")]
-        public ActionResult<List<Message>> LerMensagensAutoAck()
+        public ActionResult<List<Message>> UpConsumers()
         {
-            var factory = new ConnectionFactory { HostName = "localhost", UserName = "user", Password = "password" };
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            using var channelError = connection.CreateModel();
+            var connection = _factory.CreateConnection();
+            var channel = connection.CreateModel();
+            var channelError = connection.CreateModel();
 
 
-            channel.QueueDeclare(queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
-            channelError.QueueDeclare(queueError, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueDeclare(queue, false, false, false, null);
+            channelError.QueueDeclare(queueError, false, false, false, null);
 
-            // TODO
-            // channel.BasicQos(prefetchSize: 0, prefetchCount: 5, global: false);
+            channel.BasicQos(0, 5, false);
 
+            BuildConsumer(channel, channelError, "Consumer 1", _logger, queue, queueError);
+            BuildConsumer(channel, channelError, "Consumer 2", _logger, queue, queueError);
+            BuildConsumer(channel, channelError, "Consumer 3", _logger, queue, queueError);
+            BuildConsumer(channel, channelError, "Consumer 4", _logger, queue, queueError);
+
+            return Ok();
+        }
+
+        public static void BuildConsumer(IModel channel, IModel channelError, string WorkerName, ILogger logger, string queue, string queueError)
+        {
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
             {
+                Task.Delay(1000).Wait();
                 var body = ea.Body.ToArray();
                 try
                 {
                     var message = JsonSerializer.Deserialize<Message>(Encoding.UTF8.GetString(body));
-                    if (mensagens != null)
+                    if (message != null)
                     {
-                        // Error Aleatorios
+                        // Error Aleatorio
                         if (message.Texto.Contains("10"))
                         {
                             throw new Exception();
                         }
-                        _logger.LogInformation($"Mensagem: {message.Texto}, Hora: {message.DataEnvio}");
+                        logger.LogWarning($"Mensagem: {message.Texto}, HoraMensagem: {message.DataEnvio}, {WorkerName}, HoraConsumo: {DateTime.Now}");
                     }
                 }
                 catch (Exception ex)
@@ -81,8 +93,8 @@ namespace Rabbit.Api.Controllers
                         ErrorMessage = ex.Message
                     };
 
-                    channelError.BasicPublish(exchange: string.Empty, routingKey: queueError, basicProperties: null, body: Encoding.UTF8.GetBytes(JsonSerializer.Serialize(error)));
-                    _logger.LogError($"Message com Erro: {Encoding.UTF8.GetString(body)}");
+                    channelError.BasicPublish(string.Empty, queueError, null, Encoding.UTF8.GetBytes(JsonSerializer.Serialize(error)));
+                    logger.LogError($"Message com Erro: {Encoding.UTF8.GetString(body)}");
                 }
                 finally
                 {
@@ -90,9 +102,7 @@ namespace Rabbit.Api.Controllers
                 }
             };
 
-            channel.BasicConsume(queue: queue, autoAck: false, consumer: consumer);
-            Task.Delay(1000).Wait();
-            return Ok();
+            channel.BasicConsume(queue, false, consumer);
         }
     }
 
